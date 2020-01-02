3b2119e7f5116946df591b1dec6c42f3b1053fac
Marc Hernandez <march.masip@gmail.com>
2020-01-02 12:42:10 +0100

    exemple capses dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-header">Dashboard</div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-header">Dashboard</div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
